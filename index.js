eb90581a4c9582071367bd97f80b484891936756
va034600 <va034600@gmail.com>
2025-10-25 17:52:22 +0900

    initial

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// 静的ファイルを配信（publicフォルダにindex.htmlなどを置く）
-app.use(express.static("public"));
-
 // 例: APIエンドポイント
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from server!" });
